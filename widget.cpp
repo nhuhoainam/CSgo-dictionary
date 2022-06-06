b4be76758a8af516256405cfe59a9ddf2521025f
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2022-06-06 21:17:34 +0700

    feat(base-ui): added base UI

--- widget.cpp
+++ widget.cpp
@@ -1,3 +1,6 @@
+#include <QIcon>
+#include <QPixmap>
+
 #include "widget.h"
 #include "./ui_widget.h"
 
@@ -6,6 +9,11 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    setWindowIcon(QIcon(":/resources/img/icon.png"));
+    QPixmap hero(":/resources/img/icon2.png");
+    int w = ui->hero_pic->width(),
+        h = ui->hero_pic->height();
+    ui->hero_pic->setPixmap(hero.scaled(w, h, Qt::KeepAspectRatio));
 }
 
 Widget::~Widget()
