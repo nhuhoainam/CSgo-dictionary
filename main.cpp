57360df4f9a748b6823264f675ffd42f93ff52d3
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2022-06-22 22:50:42 +0700

    feat(sidebar): added sidebar

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include "widget.h"
+#include "sidebar.h"
 
 #include <QApplication>
 #include <QLocale>
 #include <QLabel>
+#include <QHBoxLayout>
+#include <QRect>
+
+void setupSidebar(Widget &w, Sidebar *&sidebar) {
+    w.setLayout(new QVBoxLayout());
+    w.layout()->setContentsMargins(0,0,0,0);
+    w.layout()->addWidget(sidebar);
+    QIcon *dictIcon = new QIcon(":/images/img/CSgo.png");
+    sidebar->setSidebarIcon(*dictIcon);
+    sidebar->addAction(QString("Home"), QIcon(":/images/img/home.svg"));
+    sidebar->addAction(QString("Favorite list"), QIcon(":/images/img/bookmarks.svg"));
+    sidebar->addAction(QString("History"), QIcon(":/images/img/history.svg"));
+    sidebar->addAction(QString("Edit dictionary"), QIcon(":/images/img/edit.svg"));
+    sidebar->addAction(QString("Game"), QIcon(":/images/img/game.svg"));
+    sidebar->addAction(QString("About us"), QIcon(":/images/img/people-outline.svg"));
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
     w.setWindowTitle("CSgo Dictionary");
+
+    Sidebar *sidebar = new Sidebar();
+    setupSidebar(w, sidebar);
     w.show();
     return a.exec();
 }
---
+++ sidebar.h
@@ -0,0 +1,38 @@
+#ifndef SIDEBAR_H
+#define SIDEBAR_H
+
+#include <QAction>
+#include <QWidget>
+
+class Sidebar : public QWidget {
+  Q_OBJECT
+public:
+  explicit Sidebar(QWidget *parent = nullptr);
+  void addAction(QAction *action);
+  QAction *addAction(const QString &text, const QIcon &icon = QIcon());
+  void setSidebarIcon(QIcon &icon);
+  QSize minimumSizeHint() const;
+
+signals:
+
+public slots:
+
+protected:
+  void paintEvent(QPaintEvent *event);
+  void mousePressEvent(QMouseEvent *event);
+  void mouseMoveEvent(QMouseEvent *event);
+  void leaveEvent(QEvent *event);
+
+  QAction *actionAt(const QPoint &at);
+
+private:
+  QList<QAction *> mActions;
+
+  QAction *mCheckedAction;
+  QAction *mOverAction;
+  int mSidebarWidth;
+  QSize mIconSize;
+  QIcon *mSidebarIcon;
+};
+
+#endif // SIDEBAR_H
