77b3bc804a011029879e16836abf40b5bd1d6973
Phan Lam Tung <lamtungpro1405@gmail.com>
2022-06-23 16:00:12 +0700

    Connect signals and add handling methods

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,16 @@ MainWindow::MainWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainWindow)
 {
+    setupUI();
+    connectSignalAndSlot();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setupUI() {
     ui->setupUi(this);
     container = new QStackedWidget;
     container->setObjectName("container");
@@ -16,7 +26,7 @@ MainWindow::MainWindow(QWidget *parent) :
     dictEditor = new DictionaryEditor;
     wordViewer = new SingleWordView;
     game = new Game;
-    Sidebar *sidebar = new Sidebar;
+    sidebar = new Sidebar;
 
     container->addWidget(home);
     container->addWidget(favoriteList);
@@ -38,15 +48,148 @@ MainWindow::MainWindow(QWidget *parent) :
     sidebar->addAction(QString("About us"), QIcon(":/images/img/people-outline.svg"));
 
     container->setCurrentIndex(0);
+    ui->horizontalLayout->addWidget(container);
+}
+
+void MainWindow::connectSignalAndSlot() {
     connect(sidebar,
             &Sidebar::actionSelected,
             container,
             &QStackedWidget::setCurrentIndex);
 
-    ui->horizontalLayout->addWidget(container);
+    connect(sidebar,
+            &Sidebar::actionSelected,
+            this,
+            [=](int index) {
+        switch (index) {
+        case 0:
+            handleHomeFocus();
+            break;
+        case 1:
+            handleFavoriteListFocus();
+            break;
+        case 2:
+            handleHistoryFocus();
+            break;
+        case 3:
+            handleEditorFocus();
+            break;
+        case 4:
+            handleGameFocus();
+            break;
+        }
+    });
+
+    connect(favoriteList,
+            &FavoriteList::searchRequest,
+            this,
+            &MainWindow::handleFavoriteListSearchRequest);
+    connect(favoriteList,
+            &FavoriteList::wordSelected,
+            this,
+            &MainWindow::handleFavoriteListWordSelected);
+    connect(favoriteList,
+            &FavoriteList::wordToggleFavorite,
+            this,
+            &MainWindow::handleFavoriteListWordFavorite);
+
+    connect(history,
+            &History::searchRequest,
+            this,
+            &MainWindow::handleHistorySearchRequest);
+    connect(history,
+            &History::wordSelected,
+            this,
+            &MainWindow::handleHistoryWordSelected);
+    connect(history,
+            &History::wordToggleFavorite,
+            this,
+            &MainWindow::handleHistoryWordFavorite);
+
+    connect(home,
+            &Home::wordSelected,
+            this,
+            &MainWindow::handleHomeWordSelected);
+    connect(home,
+            &Home::searchRequest,
+            this,
+            &MainWindow::handleHomeSearchRequest);
+    connect(home,
+            &Home::wordToggleFavorite,
+            this,
+            &MainWindow::handleHomeWordFavorite);
+
+    connect(dictEditor,
+            &DictionaryEditor::addNewWord,
+            this,
+            &MainWindow::handleEditorAdd);
+    connect(dictEditor,
+            &DictionaryEditor::resetDictionary,
+            this,
+            &MainWindow::handleEditorReset);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+
+void MainWindow::handleHomeSearchRequest(const QString &keyword) {
+    qDebug() << "Search " << keyword << " in home";
+}
+
+void MainWindow::handleFavoriteListSearchRequest(const QString &keyword) {
+    qDebug() << "Search " << keyword << " in favorite list";
+}
+
+void MainWindow::handleHistorySearchRequest(const QString &keyword) {
+    qDebug() << "Search " << keyword << " in history";
+}
+
+void MainWindow::handleHomeWordSelected(const QString&keyword) {
+    qDebug() << "Select " << keyword << " in home";
+}
+
+void MainWindow::handleFavoriteListWordSelected(const QString&keyword) {
+    qDebug() << "Select " << keyword << " in favorite list";
+}
+
+void MainWindow::handleHistoryWordSelected(const QString&keyword) {
+    qDebug() << "Select " << keyword << " in history";
+}
+
+void MainWindow::handleHomeWordFavorite(const QString &keyword, bool on) {
+    QString state = on ? "on" : "off";
+    qDebug() << "Toggle favorite " << keyword << state << " in home";
+}
+void MainWindow::handleFavoriteListWordFavorite(const QString &keyword, bool on) {
+    QString state = on ? "on" : "off";
+    qDebug() << "Toggle favorite " << keyword << state << " in favorite list";
+}
+void MainWindow::handleHistoryWordFavorite(const QString &keyword, bool on) {
+    QString state = on ? "on" : "off";
+    qDebug() << "Toggle favorite " << keyword << state << " in history";
+}
+
+void MainWindow::handleHomeFocus() {
+    qDebug() << "Home focus";
+}
+void MainWindow::handleFavoriteListFocus() {
+    qDebug() << "Favorite list focus";
+}
+void MainWindow::handleHistoryFocus() {
+    qDebug() << "History focus";
+}
+
+void MainWindow::handleEditorFocus() {
+    qDebug() << "Editor focus";
+}
+
+void MainWindow::handleEditorReset() {
+    qDebug() << "Reset dictionary";
+}
+
+void MainWindow::handleEditorAdd(const QString &keyword,
+                                 const QString &meaning) {
+    qDebug() << "Add new word: " << keyword << " | " << meaning;
+}
+
+void MainWindow::handleGameFocus() {
+    qDebug() << "Game focus";
 }
